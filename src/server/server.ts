5a88e5a9c418f455696de3b62b49a7f6a68c8f5b
dongnguyenltqb <dongnguyenltqb>
2022-03-13 13:16:45 +0700

    add post to rpc service

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,12 +2,14 @@ import { Server, ServerCredentials } from "@grpc/grpc-js";
 
 import { userHandler } from "./user";
 import protoDescriptor from "../protoDescriptor";
+import { postHandler } from "./post";
 
 const NewMasterServer = (addr: string) =>
   new Promise<Server>((solved, reject) => {
     const grpcServer: Server = new Server({});
     // Register server handler.
     grpcServer.addService(protoDescriptor.rpc.User.service, userHandler);
+    grpcServer.addService(protoDescriptor.rpc.Post.service, postHandler);
 
     // Bind and listen for imcomming request.
     grpcServer.bindAsync(
